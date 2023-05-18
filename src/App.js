ab1c15730ecb6b73cc41686ea431c83fde9a0a3c
santosh-chaugule <santoshchaugule1697@gmail.com>
2023-05-18 13:06:55 +0530

    OK

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import './Style.css'
+import Section from './components/layout/Section'
+import Aside from './components/layout/Aside'
+export default function App() {
+    return (
+        <>
+            <Header></Header>
+            <main><div className='container h-100 s_tbdr'>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+                <div className='row h-100'>
+                    <Aside>Left</Aside>
+                    <Section></Section>
+                    <Aside>Right</Aside>
+                </div>
+
+            </div></main>
+            <Footer></Footer>
+        </>
+    )
 }
-
-export default App;
---
+++ src/components/layout/Aside.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Aside(props) {
+    return (
+        <aside className=' col-2 s_left'>{props.children}</aside>
+    )
+}
